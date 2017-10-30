08dc16a653f14686d06e77357158d348f39a6530
Bhaskar Chandra Trivedi <32750796+BhaskarTrivedi@users.noreply.github.com>
2017-10-30 11:24:34 -0500

    Delete LoadHandFaceManualLb1113.m

--- AmericanSignLanguage/ASystemforLargeVocabularySignSearch/LoadHandFaceManualLb1113.m
+++
@@ -1,7 +0,0 @@
-function HandfaceData = LoadHandFaceManualLb1113()
-%LOADHANDFACEMANUALLB1113 Summary of this function goes here
-%   Detailed explanation goes here
-    load('handface_manual_lb1113.mat');
-    HandfaceData = handface;
-end
-
