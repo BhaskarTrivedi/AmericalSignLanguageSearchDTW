7fcb29613065728695b7a46155e43dfb45ca37d9
Bhaskar Chandra Trivedi <32750796+BhaskarTrivedi@users.noreply.github.com>
2017-10-30 11:24:14 -0500

    Delete LoadAnnotationTb1113.m

--- AmericanSignLanguage/ASystemforLargeVocabularySignSearch/LoadAnnotationTb1113.m
+++
@@ -1,8 +0,0 @@
-function AnnotationData = LoadAnnotationTb1113()
-%LOADANNOTATIONTB1113 Summary of this function goes here
-%   Detailed explanation goes here
-
-    load('annotation_tb1113.mat');
-    AnnotationData = data;
-end
-
