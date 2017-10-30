c853339a8b5cadb8e9ee296f4646708ecde2b7d2
Bhaskar Chandra Trivedi <32750796+BhaskarTrivedi@users.noreply.github.com>
2017-10-30 11:23:25 -0500

    Delete SetTranningAnnotationData.m

--- AmericanSignLanguage/ASystemforLargeVocabularySignSearch/@CASLQueryDTW/SetTranningAnnotationData.m
+++
@@ -1,7 +0,0 @@
-function Obj = SetTranningAnnotationData(Obj,Data)
-%SETTRANNINGANNOTATIONDATA Summary of this function goes here
-%   Detailed explanation goes here
-
-    Obj.TranningAnnotationData = Data;
-end
-
