bccb64e11565377e4bfe2c4487a280f68bfc8dfb
Bhaskar Chandra Trivedi <32750796+BhaskarTrivedi@users.noreply.github.com>
2017-10-30 11:23:06 -0500

    Delete SetTestAnnotationData.m

--- AmericanSignLanguage/ASystemforLargeVocabularySignSearch/@CASLQueryDTW/SetTestAnnotationData.m
+++
@@ -1,6 +0,0 @@
-function Obj = SetTestAnnotationData(Obj,Data)
-%SETTESTANNOTATIONDATA Summary of this function goes here
-%   Detailed explanation goes here
-    Obj.TestAnnotationData = Data;
-end
-
