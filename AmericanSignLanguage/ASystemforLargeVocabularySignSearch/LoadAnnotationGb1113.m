1a4a1c797b13e03ed3d21c5e22fb05e9edf351fb
Bhaskar Chandra Trivedi <32750796+BhaskarTrivedi@users.noreply.github.com>
2017-10-30 11:23:52 -0500

    Delete LoadAnnotationGb1113.m

--- AmericanSignLanguage/ASystemforLargeVocabularySignSearch/LoadAnnotationGb1113.m
+++
@@ -1,7 +0,0 @@
-function AnnotationData = LoadAnnotationGb1113()
-%LOADANNOTATIONGB113 Summary of this function goes here
-%   Detailed explanation goes here
-    load('annotation_gb1113.mat');
-    AnnotationData = data;
-end
-
