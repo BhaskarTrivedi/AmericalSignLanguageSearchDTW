b8ff3d54bf75a3ba58c529ca6e932083bb3f5d58
Bhaskar Chandra Trivedi <32750796+BhaskarTrivedi@users.noreply.github.com>
2017-10-30 11:23:36 -0500

    Delete SetTranningHandData.m

--- AmericanSignLanguage/ASystemforLargeVocabularySignSearch/@CASLQueryDTW/SetTranningHandData.m
+++
@@ -1,8 +0,0 @@
-function Obj = SetTranningHandData(Obj,HandData)
-%SETTRANNINGHANDDATA Summary of this function goes here
-%   Detailed explanation goes here
-
-    Obj.TranningHandData = HandData;
-
-end
-
